cdc799bd9e35414a1e3a6917a36abe1fed7770a3
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2025-02-02 04:37:40 +0000

    fix imports

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,8 +1,8 @@
-from .earth_surface_water import EarthSurfaceWater
+from .earth_surface_water import EarthSurfaceWater, EarthSurfaceWaterDataModule
 from .marida import MARIDA, MARIDADataModule
 from .mados import MADOS, MADOSDataModule
 from .magicbathynet import MagicBathyNet  # , MagicBathyNetDataModule
-from .ships2ais import ShipS2AIS
+from .ships2ais import ShipS2AIS, ShipS2AISDataModule
 from .swed import SWED, SWEDDataModule
 from . import transforms
 
